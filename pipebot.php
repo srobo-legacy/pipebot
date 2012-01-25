a9f002e166be9537a078f194bfb918cd469f3c53
Peter Law <PeterJCLaw@gmail.com>
2012-01-25 18:42:04 +0000

    Add a PHP wrapping class of the say script.
    This makes usage via PHP easy, and allows for a consistent interface
    when used in PHP.

---
+++ pipebot.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class to handle pumping information at pipebot.
+ */
+class Pipebot
+{
+	/**
+	 * Has pipebot say something, in a manner that doesn't block.
+	 * @param message: The string to say.
+	 */
+	public static function say($message)
+	{
+		static $s_sayPath = escapeshellarg(dirname(__FILE__).'/say');
+		$s_message = escapeshellarg($message);
+		shell_exec("$s_sayPath $s_message");
+	}
+}
