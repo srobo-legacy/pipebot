685acc14e0287c182171aa792c3170de78531688
Peter Law <PeterJCLaw@gmail.com>
2013-03-30 14:50:52 +0000

    whitespace.
    Change-Id: I3a96c9d3dbe12929b122724aa4920104f3d1e32d

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 
-HOST="irc.freenode.net"
-PORT=6667
-NICK="srpipebot-dev"
-IDENT="srpipebot-dev"
-REALNAME="srpipebot-dev"
-CHANNEL="#srobo-bots"
-PIPE_PATH="/tmp/hash-srobo"
-TARGET_BOT="srbot"
+HOST = "irc.freenode.net"
+PORT = 6667
+NICK = "srpipebot-dev"
+IDENT = "srpipebot-dev"
+REALNAME = "srpipebot-dev"
+CHANNEL = "#srobo-bots"
+PIPE_PATH = "/tmp/hash-srobo"
+TARGET_BOT = "srbot"
 
 try:
     from localconfig import *
