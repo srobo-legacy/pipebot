175a4cfa3c30012b9d95a476e8e8cb554138d187
Peter Law <PeterJCLaw@gmail.com>
2015-03-09 20:58:08 +0000

    Also make the say helper use the localconfig
    This makes it possible to configure the target pipe path for both
    sides of it.
    
    Change-Id: Ia56ec1fe84194b40b18ea22a8e44a22c5bad0a71

--- say.py
+++ say.py
@@ -1,7 +1,10 @@
 
 import os
 
-PIPE_PATH="/tmp/hash-srobo"
+try:
+    from localconfig import PIPE_PATH
+except ImportError:
+    PIPE_PATH = "/tmp/hash-srobo"
 
 def open_fifo():
     fifofd = os.open( PIPE_PATH, os.O_WRONLY | os.O_NONBLOCK )
